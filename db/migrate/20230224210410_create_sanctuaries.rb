30c7d34be62e6ceb457ad09a914e49ee6049ffeb
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-02-26 11:48:01 +0000

    schema sanctuaries 追加

--- db/migrate/20230224210410_create_sanctuaries.rb
+++ db/migrate/20230224210410_create_sanctuaries.rb
@@ -2,6 +2,15 @@ class CreateSanctuaries < ActiveRecord::Migration[6.1]
   def change
     create_table :sanctuaries do |t|
 
+t.integer :user_id, null: false
+t.integer :genre_id, null: false
+t.integer :favorite_id, null: false
+t.integer :sanctuary_tags_id, null: false
+t.float :longitude
+t.float :latitude
+t.string :name, null: false
+t.string :place, null: false
+t.string :impression, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,15 @@ ActiveRecord::Schema.define(version: 2023_02_25_210643) do
   end
 
   create_table "sanctuaries", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "genre_id", null: false
+    t.integer "favorite_id", null: false
+    t.integer "sanctuary_tags_id", null: false
+    t.float "longitude"
+    t.float "latitude"
+    t.string "name", null: false
+    t.string "place", null: false
+    t.string "impression", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
