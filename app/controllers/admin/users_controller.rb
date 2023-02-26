f6c6c6f8c57da6dc9da8ad7a2a17c9a109ba7a7d
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-02-26 14:40:00 +0000

    controller user 作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,31 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.page(params[:page])
   end
 
   def show
+    @users = User.page(params[:page])
   end
 
   def edit
+    @users = User.page(params[:page])
   end
 
   def update
+    @users = User.page(params[:page])
+    if @customer.update(customer_params)
+			   flash[:success] = "Customer was successfully updated"
+			   redirect_to admin_customer_path
+		else
+				render "edit"
+		end
   end
 
   def destroy
+  end
+
+  private
+  def customer_params
+	  params.require(:user).permit(:name,:introduction,:sns,:email,:is_deleted)
   end
 end
