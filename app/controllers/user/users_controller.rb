eb13a832a2a96474f73559e078cb30870d571e1b
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-02-27 05:54:02 +0000

    user users_controller

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,16 +1,33 @@
 class User::UsersController < ApplicationController
+
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+
+    if @user.update(user_params)
+    flash[:success] = "登録情報を変更しました"
+    redirect_to user_homes_top_path
+    else
+      render :edit and return
+    end
   end
 
   def quit
   end
 
   def out
+    @user = current_user
+    @user.update(is_deleted: true)
+
+    reset_session
+    flash[:notice] = "ありがとうございました。またのご利用ください。"
+    redirect_to root_path
   end
 end
