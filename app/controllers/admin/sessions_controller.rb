7a76ce65d80235a24d172d55edd8c9f53eb75ed7
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-03-01 13:54:52 +0000

    admin

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,11 +2,12 @@
 
 class Admin::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-    admin_root_path
+    root_path
   end
 
   def after_sign_out_path_for(resource)
-    admin_root_path
+    flash[:success] = "ログアウトしました"
+     root_path
   end
    # before_action :configure_sign_in_params, only: [:create]
 
