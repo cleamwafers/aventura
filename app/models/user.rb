e2dc182c317bb7b865e7ab271d2444a404a85758
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2023-03-12 11:43:37 +0000

    ユーザー編集画面　　画像

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@ class User < ApplicationRecord
  def active_for_authentication?
     super && (is_deleted == false)
  end
+
+  def get_image(width,heigth)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, heigth]).processed
+  end
 end
